eef136e0e7046cf02c435191b1cb0f38a08fb79f
Haoming <hmstudy02@gmail.com>
2024-01-03 09:39:55 +0800

    addressing #2
    fixing repeated installation for real this time :P

--- preload.py
+++ preload.py
@@ -1,10 +1,6 @@
-import launch
 import os
 
 EXTENSION_FOLDER = os.path.dirname(os.path.realpath(__file__))
-
-print('\nChecking Requirements for Bringing-Old-Photos-Back-to-Life...')
-
 
 FACE = os.path.join(EXTENSION_FOLDER, 'Face_Enhancement', 'checkpoints')
 if not os.path.exists(FACE):
@@ -17,14 +13,3 @@ if not os.path.exists(GLOBAL):
 MDL = os.path.join(EXTENSION_FOLDER, 'Face_Detection', 'shape_predictor_68_face_landmarks.dat')
 if not os.path.exists(GLOBAL):
     print('[Warning] face_landmarks not detected! Please download it from Release!')
-
-
-requirements = os.path.join(EXTENSION_FOLDER, 'requirements.txt')
-with open(requirements, 'r', encoding='utf8') as REQs:
-    packages = REQs.readlines()
-
-    for package in packages:
-        package = package.strip()
-
-        if not launch.is_installed(package):
-            launch.run_pip(f"install {package}", f"Old-Photo-Restoration Requirement: {package}")
